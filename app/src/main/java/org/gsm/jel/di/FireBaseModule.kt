65b89a8979605d430a16e62e03c7398d61657a66
KDY <rlaeodud9983@naver.com>
2022-06-30 21:08:07 +0900

    [CREATE] ADD Hilt di 추가

---
+++ app/src/main/java/org/gsm/jel/di/FireBaseModule.kt
@@ -0,0 +1,40 @@
+package org.gsm.jel.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.FragmentComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(FragmentComponent::class)
+object FireBaseModule {
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(): FirebaseDatabase {
+        return FirebaseDatabase.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseStorage(): FirebaseStorage {
+        return FirebaseStorage.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseStore() : FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+}
---
+++ app/src/main/java/org/gsm/jel/widget/MyApp.kt
@@ -0,0 +1,8 @@
+package org.gsm.jel.widget
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp : Application() {
+}
